841c1ecbe4b741d1cad31a81089b1cf7b754b91c
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2021-11-07 11:05:05 +0530

    Added Method to Count Consonants

--- com.String/String.c
+++ com.String/String.c
@@ -48,8 +48,22 @@ int getvowelCount(char A[]) {
 	return count;
 }
 
+int getConsonantCount(char A[]) {
+	int i, count = 0;
+	for (i = 0; A[i] != '\0'; i++) {
+		if ((A[i] >= 'a' && A[i] <= 'z') || (A[i] >= 'A' && A[i] <= 'Z')) {
+			if (!(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o'
+					|| A[i] == 'u' || A[i] == 'A' || A[i] == 'E' || A[i] == 'I'
+					|| A[i] == 'O' || A[i] == 'U')) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
 int main() {
 	char A[] = "Test String";
 	toggleCase(A);
-	printf("'%s' Vowel count is %d", A, getvowelCount(A));
+	printf("'%s'  count is %d", A, getConsonantCount(A));
 }
