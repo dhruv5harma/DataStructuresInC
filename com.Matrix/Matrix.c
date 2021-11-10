5263de7deafeea2073ce67162ae09012d0d00a27
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2021-11-10 23:53:42 +0530

    Added Displa fuction for displaying Lower triangular matrix

--- com.Matrix/Matrix.c
+++ com.Matrix/Matrix.c
@@ -30,17 +30,27 @@ void DisplayDiagonalMatrix(struct Matrix m) {
 	}
 }
 
-void setLowerTrigonalMatrix(struct Matrix *m, int i, int j, int element) {
+void setLowerTrianguarMatrix(struct Matrix *m, int i, int j, int element) {
 	if (i >= j) {
-		m->A[((i*(i-1))/2)+j-1] = element;
+		m->A[((i * (i - 1)) / 2) + j - 1] = element;
 	}
 }
 
-int getLowerTrigonalMatrix(struct Matrix m, int i, int j) {
+int getLowerTrianguarMatrix(struct Matrix m, int i, int j) {
 	if (i >= j) {
-		return m.A[((i*(i-1))/2)+j-1];
+		return m.A[((i * (i - 1)) / 2) + j - 1];
 	} else {
 		return 0;
+	}
+}
+
+void DisplayLowerTrianguarMatrix(struct Matrix m) {
+	int i, j;
+	for (i = 0; i < m.n; i++) {
+		for (j = 0; j < m.n; j++) {
+			printf("%d ", getLowerTrianguarMatrix(m, i + 1, j + 1));
+		}
+		printf("\n");
 	}
 }
 
@@ -48,11 +58,18 @@ int main() {
 
 	struct Matrix m1;
 	m1.n = 4;
-	setDiagonalMatrix(&m1, 1, 1, 10);
-	setDiagonalMatrix(&m1, 2, 2, 20);
-	setDiagonalMatrix(&m1, 3, 3, 30);
-	setDiagonalMatrix(&m1, 4, 4, 40);
-	DisplayDiagonalMatrix(m1);
+	setLowerTrianguarMatrix(&m1, 1, 1, 1);
+	setLowerTrianguarMatrix(&m1, 2, 1, 2);
+	setLowerTrianguarMatrix(&m1, 2, 2, 3);
+	setLowerTrianguarMatrix(&m1, 3, 1, 4);
+	setLowerTrianguarMatrix(&m1, 3, 2, 5);
+	setLowerTrianguarMatrix(&m1, 3, 3, 6);
+	setLowerTrianguarMatrix(&m1, 4, 1, 7);
+	setLowerTrianguarMatrix(&m1, 4, 2, 5);
+	setLowerTrianguarMatrix(&m1, 4, 3, 6);
+	setLowerTrianguarMatrix(&m1, 4, 4, 6);
+	//setDiagonalMatrix(&m1, 4, 4, 40);
+	DisplayLowerTrianguarMatrix(m1);
 
 	return 0;
 }
