7873c604164326a8a7800eb75f02ac85883eb4e9
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2021-11-12 23:45:09 +0530

    Added methods to Set Get And Display Lower Triangular matrix using Column major Formula

--- com.Matrix/Matrix.c
+++ com.Matrix/Matrix.c
@@ -54,6 +54,31 @@ void DisplayLowerTrianguarMatrix(struct Matrix m) {
 	}
 }
 
+void setLowerTrianguarColumnMajorMatrix(struct Matrix *m, int i, int j,
+		int element) {
+	if (i >= j) {
+		m->A[j * (1 - j * (j - 1) / 2) + (i - j)] = element;
+	}
+}
+
+int getLowerTrianguarColumnMajorMatrix(struct Matrix m, int i, int j) {
+	if (i >= j) {
+		return m.A[j * (1 - j * (j - 1) / 2) + (i - j)];
+	} else {
+		return 0;
+	}
+}
+
+void DisplayLowerTrianguarColumnMajorMatrix(struct Matrix m) {
+	int i, j;
+	for (i = 0; i < m.n; i++) {
+		for (j = 0; j < m.n; j++) {
+			printf("%d ", getLowerTrianguarColumnMajorMatrix(m, i + 1, j + 1));
+		}
+		printf("\n");
+	}
+}
+
 void setUpperTrianguarMatrix(struct Matrix *m, int i, int j, int element) {
 	if (i <= j) {
 		m->A[((j * (j - 1)) / 2) + i - 1] = element;
@@ -82,18 +107,31 @@ int main() {
 
 	struct Matrix m1;
 	m1.n = 4;
-	setUpperTrianguarMatrix(&m1, 1, 1, 1);
-	setUpperTrianguarMatrix(&m1, 1, 2, 2);
-	setUpperTrianguarMatrix(&m1, 1, 3, 3);
-	setUpperTrianguarMatrix(&m1, 1, 4, 4);
-	setUpperTrianguarMatrix(&m1, 2, 2, 5);
-	setUpperTrianguarMatrix(&m1, 2, 3, 6);
-	setUpperTrianguarMatrix(&m1, 2, 4, 7);
-	setUpperTrianguarMatrix(&m1, 3, 3, 5);
-	setUpperTrianguarMatrix(&m1, 3, 4, 6);
-	setUpperTrianguarMatrix(&m1, 4, 4, 6);
+	/*setUpperTrianguarMatrix(&m1, 1, 1, 1);
+	 setUpperTrianguarMatrix(&m1, 1, 2, 2);
+	 setUpperTrianguarMatrix(&m1, 1, 3, 3);
+	 setUpperTrianguarMatrix(&m1, 1, 4, 4);
+	 setUpperTrianguarMatrix(&m1, 2, 2, 5);
+	 setUpperTrianguarMatrix(&m1, 2, 3, 6);
+	 setUpperTrianguarMatrix(&m1, 2, 4, 7);
+	 setUpperTrianguarMatrix(&m1, 3, 3, 5);
+	 setUpperTrianguarMatrix(&m1, 3, 4, 6);
+	 setUpperTrianguarMatrix(&m1, 4, 4, 6);
+	 //setDiagonalMatrix(&m1, 4, 4, 40);
+	 DisplayUpperTrianguarMatrix(m1);*/
+
+	setLowerTrianguarColumnMajorMatrix(&m1, 1, 1, 1);
+	setLowerTrianguarColumnMajorMatrix(&m1, 2, 1, 2);
+	setLowerTrianguarColumnMajorMatrix(&m1, 2, 2, 3);
+	setLowerTrianguarColumnMajorMatrix(&m1, 3, 1, 4);
+	setLowerTrianguarColumnMajorMatrix(&m1, 3, 2, 5);
+	setLowerTrianguarColumnMajorMatrix(&m1, 3, 3, 6);
+	setLowerTrianguarColumnMajorMatrix(&m1, 4, 1, 7);
+	setLowerTrianguarColumnMajorMatrix(&m1, 4, 2, 8);
+	setLowerTrianguarColumnMajorMatrix(&m1, 4, 3, 9);
+	setLowerTrianguarColumnMajorMatrix(&m1, 4, 4, 10);
 	//setDiagonalMatrix(&m1, 4, 4, 40);
-	DisplayUpperTrianguarMatrix(m1);
+	DisplayLowerTrianguarColumnMajorMatrix(m1);
 
 	return 0;
 }
